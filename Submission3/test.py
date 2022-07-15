e129f8175c756de980940024950fd02d81b18b0f
Tanulr <73035034+Tanulr@users.noreply.github.com>
2022-07-15 23:58:12 +0530

    Add files via upload

--- Submission3/test.py
+++ Submission3/test.py
@@ -22,20 +22,18 @@ known_face_names = [
     "natalie"
 ]
 
-# Initialize some variables
+
 face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
 
 while True:
-    # Grab a single frame of video
+    
     ret, frame = video_capture.read()
 
     # Only process every other frame of video to save time
     if process_this_frame:
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        #small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
         small_frame = frame
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
@@ -51,11 +49,6 @@ while True:
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
-
             # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
@@ -69,11 +62,6 @@ while True:
 
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        # top *= 4
-        # right *= 4
-        # bottom *= 4
-        # left *= 4
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
